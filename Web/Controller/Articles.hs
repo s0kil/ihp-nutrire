4b90c224388ad94bdf95dd7030969d9f34df7dd8
Daniel Sokil <dtechtours@gmail.com>
2020-12-09 15:24:57 -0500

    Use IHP From Latest Master
    Fixes bug when validating is a select field item was chosen.

--- Web/Controller/Articles.hs
+++ Web/Controller/Articles.hs
@@ -65,7 +65,4 @@ buildArticle article =
     -- TODO: Validate Select Field `#categoryId`
     |> validateField #title nonEmpty
     |> validateField #text nonEmpty
-    |> validateField #categoryId nonEmptyId
-
--- Temporary, Until https://github.com/digitallyinduced/ihp/issues/596 Is Resolved
-nonEmptyId id = if id == def then Failure "Select an option" else Success
+    |> validateField #categoryId nonEmpty
