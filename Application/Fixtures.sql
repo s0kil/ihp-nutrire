bdecf01885e27d086ac7718e885b86203871101f
Daniel Sokil <dtechtours@gmail.com>
2020-12-17 11:32:37 -0500

    Added More Articles To Database Fixtures

--- Application/Fixtures.sql
+++ Application/Fixtures.sql
@@ -48,12 +48,66 @@ INSERT INTO public.articles (
     category_id
   )
 VALUES (
-    '4e279ab9-cd04-43ed-b3f3-c7b3f0461a6a',
+    '95fdca3a-3670-420e-b7ff-e0b0d4a4f767',
     'ce867693-686f-4c58-a4d2-b761266bd730',
-    'Oh Amazing Breakfast',
-    'Blah, Blah.',
-    '',
-    '2020-12-17 09:56:46.246963-05',
+    'Testing Image Default',
+    'Alright',
+    'https://picsum.photos/seed/testing-image-default/600',
+    '2020-12-17 10:48:43.619669-05',
+    '6b8bc3d1-2eeb-47eb-8885-8444a91287e8'
+  );
+INSERT INTO public.articles (
+    id,
+    user_id,
+    title,
+    text,
+    image,
+    created_at,
+    category_id
+  )
+VALUES (
+    '20fc8c54-20a8-405d-80ea-43a2f5eaa5be',
+    'ce867693-686f-4c58-a4d2-b761266bd730',
+    'Oh Amazing Breakfast Two',
+    'Random 1',
+    'https://picsum.photos/seed/oh-amazing-breakfast-two/600',
+    '2020-12-17 10:54:40.13003-05',
+    '6b8bc3d1-2eeb-47eb-8885-8444a91287e8'
+  );
+INSERT INTO public.articles (
+    id,
+    user_id,
+    title,
+    text,
+    image,
+    created_at,
+    category_id
+  )
+VALUES (
+    '8693a153-f0fb-441b-80cf-04f4dc771bd1',
+    'ce867693-686f-4c58-a4d2-b761266bd730',
+    'This Is Great!',
+    'Blah Blah',
+    'https://picsum.photos/seed/this-is-great/600',
+    '2020-12-17 11:28:04.037768-05',
+    '6b8bc3d1-2eeb-47eb-8885-8444a91287e8'
+  );
+INSERT INTO public.articles (
+    id,
+    user_id,
+    title,
+    text,
+    image,
+    created_at,
+    category_id
+  )
+VALUES (
+    '30fa1bbd-ac5b-4de5-a69c-65ce661504a2',
+    'ce867693-686f-4c58-a4d2-b761266bd730',
+    'Oh Amazing Breakfast Three',
+    'Blah Blah',
+    'https://picsum.photos/seed/oh-amazing-breakfast-three/600',
+    '2020-12-17 11:31:54.029381-05',
     '6b8bc3d1-2eeb-47eb-8885-8444a91287e8'
   );
 ALTER TABLE public.articles ENABLE TRIGGER ALL;
