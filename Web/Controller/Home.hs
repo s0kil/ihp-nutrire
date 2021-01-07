a3fc092634c8704224fb28e1956f7932e62e2ae4
Daniel Sokil <dtechtours@gmail.com>
2021-01-06 22:53:43 -0500

    Show Articles Based On Category Priority

--- Web/Controller/Home.hs
+++ Web/Controller/Home.hs
@@ -5,14 +5,25 @@ import Web.View.Home.Index
 
 instance Controller HomeController where
   action IndexAction = do
-    -- TODO: The Query Fails With Runtime Error If `votes` Table Is Empty
-    popularArticleId <- articleIdWithMostVotes
-    popularArticle <- fetch popularArticleId
+    -- TODO: The Query Fails With An Runtime Error If `votes` Table Is Empty
+    articleIdWithMostVotes :: Id Article <- do
+      let query = "SELECT article_id FROM votes GROUP BY article_id ORDER BY count(article_id) DESC LIMIT 1"
+      sqlQueryScalar query ()
+
+    popularArticle <-
+      fetch articleIdWithMostVotes
+
+    categories :: [Category] <-
+      query @Category |> fetch
+
+    -- TODO: `distinctOn #categoryId` https://github.com/digitallyinduced/ihp/issues/654
+    articles :: [Include "categoryId" Article] <-
+      query @Article
+        |> fetch
+          >>= collectionFetchRelated #categoryId
+          >>= \articles ->
+            articles
+              |> sortOn (\article -> article |> get #categoryId |> get #priority)
+              |> pure
 
     render IndexView {..}
-
-articleIdWithMostVotes :: (?modelContext :: ModelContext) => IO (Id Article)
-articleIdWithMostVotes = do
-  let query = "SELECT article_id FROM votes GROUP BY article_id ORDER BY count(article_id) DESC LIMIT 1"
-  logQuery query ()
-  sqlQueryScalar query ()
--- Web/View/Home/Index.hs
+++ Web/View/Home/Index.hs
@@ -1,13 +1,15 @@
 module Web.View.Home.Index where
 
+import Application.Helper.View
 import Web.View.Prelude
 
 data IndexView = IndexView
-  { popularArticle :: Article
+  { articles :: [Include "categoryId" Article],
+    popularArticle :: Article
   }
 
 instance View IndexView where
-  html IndexView {popularArticle} =
+  html IndexView {popularArticle, articles} =
     [hsx|
       <section class="grid grid-rows-2 grid grid-cols-1" style="width: 100%; height: 100%;">
         <div class="relative bg-cover bg-center bg-no-repeat" style="background-image: url(https://loremflickr.com/2000/1000/nutrition);">
@@ -20,5 +22,35 @@ instance View IndexView where
             </div>
           </div>
         </div>
+        <div class="grid grid-cols-4 m-auto">
+          {forEach articles renderArticle}
+        </div>
       </section>
     |]
+
+renderArticle :: Include "categoryId" Article -> Html
+renderArticle article =
+  [hsx|
+    <div class="relative">
+      <div>
+        <img class="rounded" src={imagePath} alt={get #title article}>
+      </div>
+      <div class="absolute top-0 py-6 pl-4 pr-4 w-full" style="background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0))">
+        <h1 class="text-xl text-white font-bold">
+          <a href={pathTo (ArticlesAction (get #id category))} class="border-b-2 border-yellow-400">
+            {get #name category}
+          </a>
+        </h1>
+      </div>
+      <div class="absolute bottom-0 py-6 pl-4 pr-4 w-full" style="background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5))">
+        <h1 class="text-lg text-white font-bold">
+          <a href={pathTo (ShowArticleAction (get #id article))}>
+            {get #title article}
+          </a>
+        </h1>
+      </div>
+    </div>
+  |]
+  where
+    imagePath = fromMaybe "" $ get #image article
+    category = get #categoryId article
